4de4f5017e60940ca0d4de51beba9ed0565a699f
mashaoxun <mashaoxun.lianjia.com>
2017-07-17 16:23:40 +0800

    master

--- app/main.js
+++ app/main.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Switch ,Route} from 'react-router'
-import {Index} from './containers/Index'
+// import { Provider } from 'react-redux'
+// import { createStore} from 'redux'
+// import todoApp from './reducers'
+// import App from './components/App'
+// let store  =  createStore(todoApp)
+// render(<Provider store = {store}></Provider>,document.getElementById('content'));
+
+
+
+
+import {Router, Route, hashHistory} from 'react-router'
+import Index from './containers/Index'
 import Home from './containers/Home'
-render(
-	<Switch>
-		<Route exact path="/" component = {Index} />
-		<Route path="/home" component = {Home}/ >
-	</Switch>,document.getElementById('content'));
+render((<Router history={hashHistory}>
+        <Route path='/' component={Index}></Route>
+        <Route path='/home' component={Home} />
+    	</Router>),document.getElementById('content'));
